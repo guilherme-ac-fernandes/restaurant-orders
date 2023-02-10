b48c274bf95faa4755bd570a1c58cdf723beb7a0
Guilherme Fernandes <gfengquimica@gmail.com>
2023-02-10 17:06:31 -0300

    feat(req01): criação da função para retornar análises de perguntas

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,55 @@
-def analyze_log(path_to_file):
+import csv
 
-    raise NotImplementedError
+
+def generate_amount_foods(data, person):
+    dict_food = {}
+    for name, food, _ in data:
+            if name == person:
+                if food not in dict_food:
+                    dict_food[food] = 1
+                else:
+                    dict_food[food] += 1
+    return dict_food
+
+def find_most_common_food(data, person):
+    dict_food = generate_amount_foods(data, person)
+    
+    most_order_amout = 0
+    most_order_food = ''
+    for food, amount in dict_food.items():
+        if amount >= most_order_amout:
+            most_order_amout = amount
+            most_order_food = food
+    return most_order_food
+
+
+def get_all_food_by_person(data, person):
+    all_foods = {food for _, food, _ in data}
+    all_person_food = {food for name, food, _ in data if name == person}
+    return all_foods.difference(all_person_food)
+
+def get_never_went_by_person(data, person):
+    all_days = {day for _, _, day in data}
+    all_person_day = {day for name, _, day in data if name == person}
+    return all_days.difference(all_person_day)
+
+def analyze_log(path_to_file):
+    if not path_to_file.endswith('.csv'):
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+    
+    try:
+        with open(path_to_file, encoding="utf-8") as file:
+            file_info = list(csv.reader(file, delimiter=",", quotechar='"'))
+
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
+
+    else:
+        maria_eats = find_most_common_food(file_info, 'maria')
+        arnaldo_hamburguer = generate_amount_foods(file_info, 'arnaldo')['hamburguer']
+        joao_never_ask = get_all_food_by_person(file_info, 'joao')
+        joao_never_went = get_never_went_by_person(file_info, 'joao')
+
+        result = f'{maria_eats}\n{arnaldo_hamburguer}\n{joao_never_ask}\n{joao_never_went}\n'
+        with open('data/mkt_campaign.txt', mode='w') as file_txt:
+            file_txt.write(result)
