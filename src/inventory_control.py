1f4b62b228a25d3787827ee6640dbfc23b2941a5
Guilherme Fernandes <gfengquimica@gmail.com>
2023-02-16 18:39:35 -0300

    feat: adiciona a listagem de pedidos em arquivo csv e no construtor

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class InventoryControl:
     INGREDIENTS = {
         'hamburguer': ['pao', 'carne', 'queijo'],
@@ -25,6 +28,7 @@ class InventoryControl:
             'massa': 0,
             'frango': 0,
         }
+        self.orders = []
 
     def add_new_order(self, customer, order, day):
         for ingredient in self.INGREDIENTS[order]:
@@ -32,6 +36,9 @@ class InventoryControl:
                 self.stock[ingredient] += 1
             else:
                 return False
+        row = [customer, order, day]
+        self.csv_writerow('data/invetory_orders_control.csv', row)
+        self.orders.append({customer, order, day})
 
     def get_quantities_to_buy(self):
         return self.stock
@@ -48,3 +55,8 @@ class InventoryControl:
                 available_dishes.add(dish)
 
         return available_dishes
+
+    def csv_writerow(self, path_to_file, row):
+        with open(path_to_file, 'a', encoding='UTF8') as file:
+            writer = csv.writer(file)
+            writer.writerow(row)
